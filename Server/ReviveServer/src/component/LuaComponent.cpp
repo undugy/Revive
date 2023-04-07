2afa610ff4c89ea714e4cb538837d4fb46c75462
undugy <70964651+undugy@users.noreply.github.com>
2023-04-07 17:29:40 +0900

    Lua_compnent 할당오류 수정

--- Server/ReviveServer/src/component/LuaComponent.cpp
+++ Server/ReviveServer/src/component/LuaComponent.cpp
@@ -3,27 +3,34 @@
 #include"define.h"
 #include<iostream>
 using namespace std;
-void LuaComponent::Init(const char* script_name, Enemy* volatile enemy)
+void LuaComponent::Init(const char* script_name)
 {
-	lua_State* L = m_L;
-	lock_guard<mutex>gu(this->lua_lock);
-	luaL_openlibs(L);
-	//cout << "Thread:" << this_thread::get_id() << "call" << enemy->GetID() << "lua func" << endl;
-	int error = luaL_loadfile(L, script_name) || lua_pcall(L, 0, 0, 0);
-	if (error) { cout << "Error : " << lua_tostring(L, -1); lua_pop(L, 1); }
 
-	lua_getglobal (L, "initializEnemy");
-	lua_pushnumber(L, enemy->GetID());
-	lua_pushnumber(L, enemy->GetPosX());
-	lua_pushnumber(L, enemy->GetPosY());
-	lua_pushnumber(L, enemy->GetPosZ());
-	lua_pushnumber(L, enemy->GetHP());
-	lua_pushnumber(L, enemy->GetDamge());
-	lua_pushnumber(L, CONST_VALUE::g_ground_base_pos.x);
-	lua_pushnumber(L, CONST_VALUE::g_ground_base_pos.y);
-	lua_pushnumber(L, CONST_VALUE::g_ground_base_pos.z);
-	lua_pushnumber(L, BASE_ID);
-	error = lua_pcall(L, 10, 0, 0);
+
+	lua_State*L = luaL_newstate();
+	luaL_openlibs(L);
+	
+	
+	if (luaL_loadfile(L, script_name) || lua_pcall(L, 0, 0, 0)) 
+	{ cout << "Error : " << lua_tostring(L, -1); lua_pop(L, 1); }
+	m_L = L;
+	
+}
+
+void LuaComponent::LuaInitializeEnemy(Enemy* volatile enemy)
+{
+	lua_getglobal(m_L, "initializEnemy");
+	lua_pushnumber(m_L, enemy->GetID());
+	lua_pushnumber(m_L, enemy->GetPosX());
+	lua_pushnumber(m_L, enemy->GetPosY());
+	lua_pushnumber(m_L, enemy->GetPosZ());
+	lua_pushnumber(m_L, enemy->GetHP());
+	lua_pushnumber(m_L, enemy->GetDamge());
+	lua_pushnumber(m_L, CONST_VALUE::g_ground_base_pos.x);
+	lua_pushnumber(m_L, CONST_VALUE::g_ground_base_pos.y);
+	lua_pushnumber(m_L, CONST_VALUE::g_ground_base_pos.z);
+	lua_pushnumber(m_L, BASE_ID);
+	int error = lua_pcall(m_L, 10, 0, 0);
 	if (error)
 		LuaErrorDisplay(error);
 
@@ -48,11 +55,11 @@ void LuaComponent::LuaErrorDisplay(int err_num)
 
 void LuaComponent::CallStateMachine(Enemy* enemy)
 {
-	//lock_guard<mutex>gu(this->lua_lock);
+	
 	lua_getglobal(m_L, "state_machine");
 	lua_pushnumber(m_L, enemy->GetTargetId());
 	int err = lua_pcall(m_L, 1, 0, 0);
-	//if (err)
-	//	LuaErrorDisplay(err);
+	if (err)
+		LuaErrorDisplay(err);
 
 }
